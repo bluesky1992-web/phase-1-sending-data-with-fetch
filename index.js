a85cd357a2eedeab800736f83b1c861f41e02056
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-01-14 03:03:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Add your code here
 // Add your code here
 
-function submitData(userName, userEmail) {
+function submitData(name, email) {
   const user = {
-    name: userName,
-    email: userEmail,
+    name,
+    email,
   };
   const configurationObject = {
     method: "POST",
     headers: {
-      "Content-type": "application/json", 
+      "Content-type": "application/json",
       Accept: "application/json",
     },
     body: JSON.stringify(user),
@@ -17,13 +17,11 @@ function submitData(userName, userEmail) {
   const fetchingData = fetch("http://localhost:3000/users", configurationObject)
     .then((res) => res.json())
     .then((data) => {
-      const h1 = document.querySelector("h1");
-      h1.innerText = data.id;
+      document.body.innerHTML = data["id"];
     })
 
-    .catch(error => {
-        const p = document.querySelector('p')
-        p.innerText = error.message
-    })
+    .catch((error) => {
+      document.body.innerHTML = error.message;
+    });
   return fetchingData;
 }
